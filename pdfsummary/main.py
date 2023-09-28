4b41d45ad9692a4485fb349d06aa933c70f7b978
sayalee damle <sayalee.damle@onepointltd.com>
2023-09-28 22:00:57 +0530

    added functionality

--- pdfsummary/main.py
+++ pdfsummary/main.py
@@ -3,15 +3,17 @@ import extractkeyword as extractk
 import vector_embedding as ve
 from pathlib import Path
 
+
+
 @cl.on_chat_start
-async def start():
+async def start() -> cl.Message:
 
     path_pdf = None
 
 
     while path_pdf == None:
         path_pdf = await cl.AskUserMessage(
-            content=f"enter path of PDF", timeout=15
+            content=f"enter path of PDF", timeout = 15
         ).send()
        
     #after we get the Path
@@ -19,7 +21,7 @@ async def start():
     summary = await extractk.summary_llm(pages)
     
     await cl.Message(
-        content=f"The summary of the PDF file is: {summary}"
+        content=f"{summary}"
     ).send()
 
     ans = None
@@ -40,5 +42,39 @@ async def start():
         await cl.Message(
             content=f"The translation of the summary file is: {translation}"
         ).send()
+    else:
+        await cl.Message(
+                content=f"okay"
+            ).send()
     
+    choiceq= None
+    
+    while choiceq == None:
+        choiceq = await cl.AskUserMessage(
+                    content=f"Do you have any questions about this? (Yes/No)", timeout=15
+                ).send()
+        
+    if choiceq['content'] in ("yes", "y", "Yes", "Y"):
+        f = True
+        while f == True:
+            ques = None
+            while ques == None:
+                ques = await cl.AskUserMessage(
+                        content=f"What is the Question?", timeout=15
+                    ).send()
+                
+            answer = await extractk.question_llm(ques['content'], db)
+            await cl.Message(
+                content=f"{answer}"
+            ).send()
+            c = None
+            while c == None:
+                c = await cl.AskUserMessage(
+                        content=f"Do you have more questions (Yes/No)", timeout=15
+                    ).send()
+            if c not in ("yes", "y", "Yes", "Y"):
+                f = False
 
+    await cl.Message(
+                content=f"Thank You!"
+            ).send()
--- pdfsummary/vector_embedding.py
+++ pdfsummary/vector_embedding.py
@@ -1,6 +1,6 @@
 
-from langchain.document_loaders import UnstructuredPDFLoader
-
+from langchain.document_loaders import UnstructuredPDFLoader, PyPDFLoader, PyPDFium2Loader
+from PyPDF2 import PdfReader, PdfWriter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import  FAISS
@@ -13,6 +13,7 @@ import pickle
 
 # if embedding dir is empty
 async def create_vector_store(doc_to_split) -> FAISS:
+    print("in create_vector_store")
     text_splitter = CharacterTextSplitter(chunk_size=cfg.split_size, chunk_overlap=10)
     documents = text_splitter.split_documents(doc_to_split)
     db = FAISS.from_documents(documents, cfg.emb_func)
@@ -21,27 +22,32 @@ async def create_vector_store(doc_to_split) -> FAISS:
 
 #check if embedding dir exists else create
 async def init_vector_store(path_pdf: Path) -> Tuple[FAISS, List[Document]]:
+    print("in init")
     file_path = cfg.path_embedding_dir/path_pdf.stem    #create a folder below path_embedding_dir
     documents_path = cfg.path_embedding_dir/f"{path_pdf.stem}_document"
+    print(documents_path)
     if file_path.exists() and len(list(file_path.glob("*"))) > 0:
+        print("in if")
         db = FAISS.load_local(file_path.as_posix(), cfg.emb_func)
         with open(documents_path, 'rb') as f:
             documents = pickle.load(f)
             return db, documents
     else:
-        loader = UnstructuredPDFLoader(path_pdf)
+        print("in else")
+        loader = PyPDFium2Loader(str(path_pdf))
         pages = loader.load()
-        db = create_vector_store(pages)
+        print("Got pages")
+        db = await create_vector_store(pages)
         with open(documents_path, 'wb') as f:
             pickle.dump(pages, f)
         if not file_path.exists():
             file_path.mkdir(parents = True)
         db.save_local(file_path.as_posix())
-        return db, pages
+        return db, pages[0]
 
 if __name__ == "__main__":
-    path_pdf = Path("C:/Users/Sayalee/Documents/samplepdf2.pdf")
-    faiss, documents = init_vector_store(path_pdf) 
+    path_pdf = Path("/Users/sayaleedamle/WorkDocumets/samplepdf3.pdf")
+    faiss, documents =  init_vector_store(path_pdf) 
     assert faiss is not None
     assert documents is not None
     assert len(documents) > 0
