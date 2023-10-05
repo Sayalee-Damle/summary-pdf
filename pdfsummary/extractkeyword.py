1d8023eca2e70b2baab3b868e1592c3d96e0b60e
sayalee damle <sayalee.damle@onepointltd.com>
2023-10-05 12:21:53 +0530

    code clean up

--- pdfsummary/extractkeyword.py
+++ pdfsummary/extractkeyword.py
@@ -14,53 +14,59 @@ from pathlib import Path
 class CommaSeparatedListOutputParser(BaseOutputParser):
     def parse(self, text: str):
         return text.strip().split(", ")
-        
+
+
+def prompt_factory(system_template, human_template):
+    system_message_prompt = SystemMessagePromptTemplate.from_template(
+        template=system_template
+    )
+    human_message_prompt = HumanMessagePromptTemplate.from_template(
+        template=human_template
+    )
+    messages = [system_message_prompt, human_message_prompt]
+    chat_prompt = ChatPromptTemplate.from_messages(messages)
+    return chat_prompt
 
 
 def get_db_pages(path_pdf):
-    #path_pdf = user_input.get_path_pdf()
+    # path_pdf = user_input.get_path_pdf()
     db, pages = asyncio.run(ve.init_vector_store(path_pdf))
     return db, pages
 
 
-#to find summary
+# to find summary
 async def summary_llm(pages):
     text = ve.convert_to_text(pages)
-    system_message_prompt = SystemMessagePromptTemplate.from_template(t.summary_template)
-    human_template = "{text}"
-    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
-
-    chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
-
-    chain = LLMChain(llm = Config.llm, prompt = chat_prompt)
-    summary = await chain.arun({'text': text})
+    chat_prompt = prompt_factory(t.summary_template, "{text}")
+    chain = LLMChain(llm=Config.llm, prompt=chat_prompt)
+    summary = await chain.arun({"text": text})
     return summary
 
-async def translation(summary, language):
-    system_message_prompt_3 = SystemMessagePromptTemplate.from_template(t.system_translate_template)
-    human_message_prompt_3 = HumanMessagePromptTemplate.from_template(t.translate_template)
-    chat_prompt_3 = ChatPromptTemplate.from_messages([system_message_prompt_3, human_message_prompt_3])
-    chain3 = LLMChain(llm = Config.llm, prompt = chat_prompt_3)
-    translate =await chain3.arun({'summary': summary, 'language': language})
-    return translate
-   
-#print(pages[0].page_content)
 
-#template for question answer
+async def translation(summary, language):
+    chat_prompt_3 = prompt_factory(t.system_translate_template, t.translate_template)
+    chain3 = LLMChain(llm=Config.llm, prompt=chat_prompt_3)
+    translate = await chain3.arun({"summary": summary, "language": language})
+    return translate
+
+
+# print(pages[0].page_content)
+
+
+# template for question answer
 async def question_llm(query, db):
-    system_message_prompt_2 = SystemMessagePromptTemplate.from_template(t.system_template)
-    human_message_prompt_2 = HumanMessagePromptTemplate.from_template(t.template2)
-    chat_prompt_2 = ChatPromptTemplate.from_messages([system_message_prompt_2, human_message_prompt_2])
+    chat_prompt_2 = prompt_factory(t.system_template, t.template2)
     docs = db.similarity_search(query)
     ve.convert_to_text(docs)
-    chain2 = LLMChain(llm = Config.llm, prompt = chat_prompt_2)
+    chain2 = LLMChain(llm=Config.llm, prompt=chat_prompt_2)
     ans = await chain2.arun({"output": docs, "question": query})
     return ans
 
 
 if __name__ == "__main__":
     import asyncio
+
     path_pdf = Path("C:/Users/Sayalee/Documents/langchain_research_paper.pdf")
     db, pages = asyncio.run(ve.init_vector_store(path_pdf))
     print(asyncio.run(summary_llm(pages)))
-    #print(pages)
+    # print(pages)
